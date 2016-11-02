b7e27a36afc0f98d63bc37e719f89d9814728bf4
ansuz <ansuz@transitiontech.ca>
2016-11-02 21:10:23 +0100

    handle more cases. add tests

--- index.js
+++ index.js
@@ -7,17 +7,13 @@ var countColons = function (x) {
 var padIPv6 = function (ip) {
     return ip
     .replace(/::/, function (two) {
-        return ':' + Array(5-countColons(two)).fill('0000').join(':') + ':';
+        return ':' + Array((7 - countColons(ip)) + 1).join(':') + ':';
     })
     .split(':')
     .map(function (x) {
         return Array(4-x.length).fill('0').join("") + x;
     })
-    .join(':')
-    .replace(/^.*$/, function (all) {
-        while (all.length < 39) { all += ":0000"; }
-        return all;
-    });
+    .join(':');
 };
 
 module.exports = padIPv6;
--- test.js
+++ test.js
@@ -1,24 +1,26 @@
 var pad = require("./index");
 
-
 var countColons = function (x) {
     var n = 0;
     x.replace(/:/g, function (c) { n++; });
     return n;
 };
 
-//var shouldEqual = "fe80:0000:0000:0000:4685:00ff:fe72:c4fa";
-
 [
-    "fc00::",
-    "fe80:685::ff:fe72:c4fa",
-].forEach(function (ip) {
-    var padded = pad(ip);
+    ["fc00::",                  "fc00:0000:0000:0000:0000:0000:0000:0000"],
+    ["fe80:685::ff:fe72:c4fa",  "fe80:0685:0000:0000:0000:00ff:fe72:c4fa"],
+    ["::",                      "0000:0000:0000:0000:0000:0000:0000:0000"],
+    ["::1",                     "0000:0000:0000:0000:0000:0000:0000:0001"],
+].forEach(function (pair) {
+    var input = pair[0];
+    var expected = pair[1];
 
-    console.log({
-        input: ip,
-        inputLength: ip.length,
-        inputColons: countColons(ip),
+    var padded = pad(input);
+
+    false && console.log({
+        input: input,
+        inputLength: input.length,
+        inputColons: countColons(input),
         output: padded,
         outputLength: padded.length,
         outputColons: countColons(padded),
@@ -26,8 +28,7 @@ var countColons = function (x) {
 
     if (countColons(padded) !== 7) { throw new Error("Expected more colons"); }
     if (padded.length !== 39) { throw new Error("Expected a 39 character IP"); }
-
-    console.log();
+    if (padded !== expected) { throw new Error(padded + " !== " + expected); }
 });
 
 
